d9e21dc72d92b825293bce71cd2b8d551f6c4f8c
sixthson6 <108383344+sixthson6@users.noreply.github.com>
2025-01-04 14:31:08 +0000

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: (string | undefined | null | false)[]) {
-  return twMerge(clsx(inputs));
-}
-// export function cn(...inputs: ClassValue[]) {
-//   return twMerge(clsx(inputs))
-// }
